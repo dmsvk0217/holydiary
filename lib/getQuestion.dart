49f4850e52cc52e1357575089f0b94ecdc9be36e
Zho <chlwlgh1011@naver.com>
2023-03-25 18:41:51 +0900

    connect open_ai but Null error_

---
+++ lib/getQuestion.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:holydiary/resultPage.dart';
+
+class GetQuestion extends StatefulWidget {
+  const GetQuestion({Key? key}) : super(key: key);
+
+  @override
+  _GetQuestionState createState() => _GetQuestionState();
+}
+
+class _GetQuestionState extends State<GetQuestion> {
+  final TextEditingController _controller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        TextField(
+          controller: _controller,
+        ),
+        TextButton(
+          onPressed: () {
+            String prompt = _controller.text;
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => ResultPage(prompt)));
+          },
+          child: const Text("Get result"),
+        ),
+      ]),
+    );
+  }
+}
