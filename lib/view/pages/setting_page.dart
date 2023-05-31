2d8170fa946f2d56468a6f357044f850c93464fd
Zho <chlwlgh1011@naver.com>
2023-05-31 23:07:57 +0900

    setting_page_layout

--- lib/view/pages/setting_page.dart
+++ lib/view/pages/setting_page.dart
@@ -1,13 +1,49 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:holydiary/view/resources/color_manager.dart';
+import 'package:holydiary/view/resources/font_manager.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("SettingPage"),
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            children: [
+              const SizedBox(
+                width: 32,
+              ),
+              Text(
+                "고객센터",
+                style: TextStyle(
+                    fontSize: FontSize.s14,
+                    color: ColorManager.text,
+                    fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Container(
+            width: 350,
+            height: 150,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: ColorManager.textfield,
+            ),
+            child: const Column(
+              children: [
+                Text("data"),
+                Text("data"),
+                Text("data"),
+                Text("data"),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
