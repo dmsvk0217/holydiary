56c25dacc3a42c914586dc1c70d369487c1fd245
Zho <chlwlgh1011@naver.com>
2023-06-01 11:18:22 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/route_manager.dart';
 import 'package:holydiary/view/resources/getx_routes_manager.dart';
 import 'package:holydiary/view/resources/theme_manager.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 import 'firebase_options.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -30,47 +28,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      localizationsDelegates: [
+      localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: [
-        const Locale('ko', ''), // Add Korean locale
+      supportedLocales: const [
+        Locale('ko', ''), // Add Korean locale
       ],
       debugShowCheckedModeBanner: false,
       title: "HolyDiary",
       theme: getApplicationTheme(),
-      initialRoute: Routes.authPage,
+      initialRoute: Routes.getQuestionRoute,
       getPages: getPages,
     );
   }
-}
-
-Future<String> generateText(String prompt) async {
-  String generatedText = "";
-  final response = await http.post(
-    Uri.parse(apiUrl),
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer $openaikey'
-    },
-    body: jsonEncode({
-      "model": "text-davinci-003",
-      'prompt':
-          "What is $prompt? Tell me like you're explaining to an eight-year-old.",
-      'max_tokens': 100,
-      'temperature': 0,
-      'top_p': 1,
-      'frequency_penalty': 0,
-      'presence_penalty': 0
-    }),
-  );
-  if (response.statusCode == 200) {
-    var data = jsonDecode(response.body);
-    generatedText = data['choices'][0]['text'];
-  } else {
-    generatedText = "Error: ${response.reasonPhrase}";
-  }
-  return generatedText;
 }
